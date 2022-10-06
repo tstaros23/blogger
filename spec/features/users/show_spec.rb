84d254044550212bf00716bcefd9bfd5a6b5682b
Ted Staros <tstaros23@gmail.com>
2022-10-06 13:19:39 -0600

    Fix test and add Javascript dialogue when delete button is clicked

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -18,14 +18,14 @@ end
    describe 'they link from the show page' do
      it "displays all the articles without the deleted entry" do
        article_1 = Article.create!(title: "Title 1", body: "Body 1")
-       article_1 = Article.create!(title: "Title 2", body: "Body 2")
+       article_2 = Article.create!(title: "Title 2", body: "Body 2")
 
        visit article_path(article_1)
        click_on "Delete"
 
        expect(current_path).to eq(articles_path)
-       expect(current_path).to eq(article_2.title)
-       expect(current_path).to eq(article_1.title)
+       expect(page).to have_content(article_2.title)
+       expect(page).to_not have_content(article_1.title)
      end
    end
  end
